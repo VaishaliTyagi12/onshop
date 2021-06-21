2fe0e4f56868f2291898dbaf55954443751bb871
VaishaliTyagi12 <84836181+VaishaliTyagi12@users.noreply.github.com>
2021-06-21 19:51:54 +0530

    Add project files.

---
+++ onshop/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using onshop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onshop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private ApplicationDbContext _dbcontext;
+        public ProductsController(ApplicationDbContext dbContext)
+        {
+            _dbcontext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
